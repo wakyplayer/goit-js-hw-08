bece5f8178793e35bfb08c0bc1774d3f4140956a
Dmitry Chebanov <chebanov.d09@gmail.com>
2025-06-27 07:01:53 +0300

    update

--- js/task-3.js
+++ js/task-3.js
@@ -1,32 +1,7 @@
-class StringBuilder {
-  #value;
+const inputEl = document.querySelector("#name-input");
+const outputEl = document.querySelector("#name-output");
 
-  constructor(initialValue) {
-    this.#value = initialValue;
-  }
-
-  getValue() {
-    return this.#value;
-  }
-
-  padEnd(str) {
-    this.#value += str;
-  }
-
-  padStart(str) {
-    this.#value = str + this.#value;
-  }
-
-  padBoth(str) {
-    this.#value = str + this.#value + str;
-  }
-}
-
-const builder = new StringBuilder(".");
-console.log(builder.getValue()); // "."
-builder.padStart("^");
-console.log(builder.getValue()); // "^."
-builder.padEnd("^");
-console.log(builder.getValue()); // "^.^"
-builder.padBoth("=");
-console.log(builder.getValue()); // "=^.^="
+inputEl.addEventListener("input", (event) => {
+  const trimmedValue = event.currentTarget.value.trim();
+  outputEl.textContent = trimmedValue === "" ? "Anonymous" : trimmedValue;
+});
