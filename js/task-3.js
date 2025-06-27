7180325a74c75dacde056b58551211c809645a9c
Dmitry Chebanov <chebanov.d09@gmail.com>
2025-06-27 06:25:23 +0300

    update

--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,24 @@
-function filterArray(numbers, value) {
-  const result = [];
+const profile = {
+  username: "Jacob",
+  playTime: 300,
 
-  for (let number of numbers) {
-    if (number > value) {
-      result.push(number);
-    }
-  }
+  changeUsername(newName) {
+    this.username = newName;
+  },
 
-  return result;
-}
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+};
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
