f2da61efe396bf49d374255dddced10f79d2ff9f
Dmitry Chebanov <chebanov.d09@gmail.com>
2025-06-27 07:11:39 +0300

    update

--- js/task-3.js
+++
@@ -1,7 +0,0 @@
-const inputEl = document.querySelector("#name-input");
-const outputEl = document.querySelector("#name-output");
-
-inputEl.addEventListener("input", (event) => {
-  const trimmedValue = event.currentTarget.value.trim();
-  outputEl.textContent = trimmedValue === "" ? "Anonymous" : trimmedValue;
-});
